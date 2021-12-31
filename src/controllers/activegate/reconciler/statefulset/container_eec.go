83fda26575e89b3ab7f04a90e04d0678c5a0600b
Tomasz Szreder <Tomasz.Szreder@dynatrace.com>
2021-12-31 13:19:59 +0100

    Create EEC/StatsD config map with runtimeProperties to be mounted in EEC container (corresponds to feature flags but OneAgent-like)

--- src/controllers/activegate/reconciler/statefulset/container_eec.go
+++ src/controllers/activegate/reconciler/statefulset/container_eec.go
@@ -2,6 +2,7 @@ package statefulset
 
 import (
 	"fmt"
+	"regexp"
 
 	"github.com/Dynatrace/dynatrace-operator/src/controllers/activegate/internal/consts"
 	"github.com/Dynatrace/dynatrace-operator/src/kubeobjects"
@@ -85,7 +86,26 @@ func (eec *ExtensionController) BuildVolumes() []corev1.Volume {
 				EmptyDir: &corev1.EmptyDirVolumeSource{},
 			},
 		},
+		{
+			Name: "eec-config",
+			VolumeSource: corev1.VolumeSource{
+				ConfigMap: &corev1.ConfigMapVolumeSource{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: BuildEecConfigMapName(eec.StsProperties.Name, eec.StsProperties.feature),
+					},
+				},
+			},
+		},
 	}
+}
+
+func BuildEecConfigMapName(instanceName string, module string) string {
+	if len(instanceName) == 0 || len(module) == 0 {
+		err := fmt.Errorf("empty instance or module name not allowed (instance: %s, module: %s)", instanceName, module)
+		log.Error(err, "problem building EEC config map name")
+		return ""
+	}
+	return regexp.MustCompile(`[^\w\-]`).ReplaceAllString(instanceName+"-"+module+"-eec-config", "_")
 }
 
 func (eec *ExtensionController) image() string {
@@ -118,6 +138,7 @@ func (eec *ExtensionController) buildVolumeMounts() []corev1.VolumeMount {
 		{Name: dataSourceMetadata, MountPath: "/opt/dynatrace/remotepluginmodule/agent/datasources/statsd", ReadOnly: true},
 		{Name: "extensions-logs", MountPath: extensionsLogsDir},
 		{Name: "statsd-logs", MountPath: statsDLogsDir, ReadOnly: true},
+		{Name: "eec-config", MountPath: "/var/lib/dynatrace/remotepluginmodule/agent/conf/runtime"},
 	}
 }
 
